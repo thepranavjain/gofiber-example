e7a68450d3785695744b812fa258d00382b244d2
thepranavjain <pranavjn.kota@gmail.com>
2022-08-27 03:10:43 +0530

    routing with url params

--- main.go
+++ main.go
@@ -7,9 +7,18 @@ func main() {
 
 	app.Get("/", handler)
 
+	app.Get("/with-param/:name?", withParamHandler)
+
 	app.Listen(":3000")
 }
 
 func handler(c *fiber.Ctx) error {
 	return c.SendString("Hello World!")
+}
+
+func withParamHandler(c *fiber.Ctx) error {
+	if c.Params("name") != "" {
+		return c.SendString("Hello " + c.Params("name"))
+	}
+	return c.SendString("Who are you?")
 }
